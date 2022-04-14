84d01eca85adc9376a409109a9f8a5c70eda47eb
sun00120 <sun00120@algonquinlive.com>
2022-04-14 17:52:08 -0400

    modified http_helper.dart file

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -8,7 +8,21 @@ class HttpHelper {
   String? token = null;
 
   Map<String, String> headers = {
+    'x-api-key': 'JiaqianHygor',
     'Content-type': 'application/json, charset=UTF-8',
-    'Accept': 'application/json',
   };
+
+  var prefs;
+
+  void intHeaders() async {
+    prefs = prefs ?? await SharedPreferences.getInstance();
+    token = prefs.getString('token');
+    headers.update('Authorization', (value) => 'Bearer $token');
+  }
+
+  void updatetoken() async {
+    prefs = prefs ?? await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+    headers.update('Authorization', (value) => 'Bearer $token');
+  }
 }
