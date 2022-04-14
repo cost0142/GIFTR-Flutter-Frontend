b25317040ab41645b454d800d05447b868f8830e
sun00120 <sun00120@algonquinlive.com>
2022-04-14 18:52:59 -0400

    update pubspec.lock, pubspec.yaml and add POST req. LOGIN

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:async';
-
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HttpHelper {
-  final String domain = 'http://localhost:3030';
+  final String domain = '';
   String? uid = null;
   String? token = null;
 
+  //header
   Map<String, String> headers = {
     'x-api-key': 'JiaqianHygor',
     'Content-type': 'application/json, charset=UTF-8',
   };
 
-  var prefs;
+  var preferences;
 
+  //get token
   void intHeaders() async {
-    prefs = prefs ?? await SharedPreferences.getInstance();
-    token = prefs.getString('token');
+    preferences = preferences ?? await SharedPreferences.getInstance();
+    token = preferences.getString('token');
     headers.update('Authorization', (value) => 'Bearer $token');
   }
 
-  void updatetoken() async {
-    prefs = prefs ?? await SharedPreferences.getInstance();
-    await prefs.setString('token', token);
+  //update token
+  void updateToken() async {
+    preferences = preferences ?? await SharedPreferences.getInstance();
+    await preferences.setString('token', token);
     headers.update('Authorization', (value) => 'Bearer $token');
+  }
+
+  //login
+  Future<String> Login(String email, String password) async {
+    String endpoint = 'auth/tokens';
+    Uri uri = Uri.http(domain, endpoint);
+
+    //body Req.
+    Map<String, String> body = {
+      'email': email,
+      'password': password,
+    };
+
+    // POST Req.
+    http.Response response = await http.post(uri, body: body);
+    Map<String, dynamic> resp = jsonDecode(response.body);
+    if (resp['data'] != null) {
+      String _token = resp['data']['token'];
+      updateToken(_token);
+      return _token;
+    } else {
+      String msg = '${resp['errors'][0]['code']} ${resp['errors'][0]['title']}';
+      throw Exception(msg);
+    }
   }
 }
